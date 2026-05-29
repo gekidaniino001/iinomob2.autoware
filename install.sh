7bd5519875d2f49cbbaac47d67ac4010c5f19cee
Masao KONDOH <kondoh@axe.bz>
2026-05-29 20:14:38 +0900

    feat(install.sh): add setuptools version check, etc.

--- install.sh
+++ install.sh
@@ -18,6 +18,18 @@ rosdep update
 
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 
+PKG="setuptools"
+VER_TGT="59.6.0"
+VER_NOW=$( pip show $PKG | grep Version | tr -d ' ' | cut -d : -f2 )
+if [ "$VER_NOW" != "$VER_TGT" ]; then
+  pip install $PKG==$VER_TGT >/dev/null
+fi
+
+### for test
+## cat ~/test_patch/d-univ2 | ( cd src/iino.universe ; patch -p1 )
+
+## colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-works 4
+## colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --packages-select autoware_launch
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 pushd src/iino.universe/tool
@@ -26,3 +38,6 @@ popd
 
 mkdir -p src/iino.scenario/data_bin
 cp -r ~/enkaku src/iino.scenario/data_bin/
+
+$TOOL_DIR/lan_setup.py
+$TOOL_DIR/ssd_setup.py
